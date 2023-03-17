ccf561e1cd570cf975a648f1c06c15f7187dba1a
Erika <erikaguardo99@gmail.com>
2023-03-17 22:34:45 +0100

    footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,90 +1,103 @@
 import "../assets/styles.scss";
 import axios from "axios";
 //import _get lodash
-const get = require('lodash.get');
-
-
+const get = require("lodash.get");
 
 //on page loading show the spinner
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        document.querySelector(
-        "body").style.visibility = "hidden";
-        document.getElementById(
-        "loader").style.visibility = "visible";
-    } else {
-        document.getElementById(
-        "loader").style.display = "none";
-        document.querySelector(
-        "body").style.visibility = "visible";
-    }
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.getElementById("loader").style.visibility = "visible";
+  } else {
+    document.getElementById("loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
 };
 
+//function for Typewriter effect
 
+const elementEl = document.getElementById("typeText");
+
+const typewriter = new Typewriter(elementEl, {
+  loop: true,
+});
+
+typewriter
+  .typeString("Latest News")
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString("Get access to the latest news about technology,")
+  .pauseFor(2500)
+  .deleteChars(11)
+  .typeString(" games,")
+  .pauseFor(2500)
+  .deleteChars(6)
+  .typeString(" blogs,")
+  .pauseFor(2500)
+  .deleteChars(6)
+  .typeString(" cloud computing.")
+  .pauseFor(2500)
+  .deleteAll()
+  .start();
 
 //declaring variables
-const newsStoriesUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
-const idsUrl = 'https://hacker-news.firebaseio.com/v0/item/';
+const newsStoriesUrl =
+  "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+const idsUrl = "https://hacker-news.firebaseio.com/v0/item/";
 
 let idsArr = [];
-let startArr= 0;
+let startArr = 0;
 let endArr = 10;
 
 const div = document.getElementById("addNews");
 
 //function to fetch news array and ids
 
-async function getIdsNews (){
-try{
-    let res= await axios.get(newsStoriesUrl);
+async function getIdsNews() {
+  try {
+    let res = await axios.get(newsStoriesUrl);
 
-let arr= res.data; 
-console.log(arr);
-for (let i = startArr; i < endArr && i< arr.length; i++) {
-    // get the size of the inner array
-     idsArr = arr[i];
-     getNews()
-     console.log(idsArr);
+    let arr = res.data;
+    console.log(arr);
+    for (let i = startArr; i < endArr && i < arr.length; i++) {
+      // get the size of the inner array
+      idsArr = arr[i];
+      getNews();
+      console.log(idsArr);
+    }
+    if (endArr > arr.length) {
+      startArr = 0;
+      endArr = 10;
+    } else {
+      startArr = endArr;
+      endArr += 10;
+    }
+  } catch {
+    err => {
+      console.log(err);
+    };
+  }
 }
-if (endArr> arr.length){
-    startArr= 0;
-    endArr= 10;
-} else{
-    startArr = endArr;
-    endArr += 10 ;
-}
- 
 
-
-
-}
-catch{(err)=>{console.log(err)};}
-
-
-}
 //function to get specific properties and post them in cards
 
-async function getNews (){
-try{let url = `${idsUrl + idsArr + '.json'}`
-axios.get(url)
-.then((res)=> {
-    let title= get(res, 'data.title');
-    let date = get(res, 'data.time');
-    let link = get(res, 'data.url');
-   if (link == undefined){
-    document.getElementById("linkTo").addEventListener('click', function(event)
-    {event.preventDefault()})
-   }
+async function getNews() {
+  try {
+    let url = `${idsUrl + idsArr + ".json"}`;
+    axios.get(url).then(res => {
+      let title = get(res, "data.title");
+      let date = get(res, "data.time");
+      let link = get(res, "data.url");
 
-    console.log(title,date,link)
-    
-   //convert string to a date object
-    const fullDate = new Date(date * 1000);
-    const finalDate=  fullDate.toLocaleDateString('en-GB');
-    console.log(finalDate);
+      console.log(title, date, link);
 
-    
-    div.innerHTML += `<div class="card mx-3 my-2" style="width: 18rem;">
+      //convert string to a date object
+      const fullDate = new Date(date * 1000);
+      const finalDate = fullDate.toLocaleDateString("en-GB");
+      console.log(finalDate);
+
+      if (link != undefined) {
+        div.innerHTML += `<div class="card mx-3 my-2" id="card">
     <div class="card-body">
       <h5 class="card-title">${title}</h5>
      <p class="card-text">
@@ -92,64 +105,81 @@ axios.get(url)
 
     </div>
     <div class="card-footer "> Last updated: ${finalDate} </div>
- </div> `
-
-    
-
-
-})
- } 
- catch{(err)=>{console.log(err)};}
+ </div> `;
+      } else {
+        return null;
+      }
+    });
+  } catch {
+    err => {
+      console.log(err);
+    };
+  }
 }
-
 
 getIdsNews();
 
-
- 
-
-
-
 //load more news (button)
 const btn = document.getElementById("showMoreBtn");
-window.onscroll= function (){loadMore(), scrollFunction()};
-function loadMore () {
-
-if (document.body.scrollTop > 100 || 
-    document.documentElement.scrollTop > 100) {
+window.onscroll = function () {
+  loadMore(), scrollFunction(), displayFooter();
+};
+function loadMore() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
     btn.style.display = "block";
   } else {
     btn.style.display = "none";
   }
-
-
 }
-btn.onclick = function (){getIdsNews(), spinnerOnload() };
+btn.onclick = function () {
+  getIdsNews(), spinnerOnload();
+};
 
 //add load more button's spinner
-let spinner= document.getElementById("spinner");
+let spinner = document.getElementById("spinner");
 
- function spinnerOnload (){
+function spinnerOnload() {
   spinner.classList.remove("hidden");
-  setTimeout(()=> {spinner.classList.add("hidden");}, 2000);
+  setTimeout(() => {
+    spinner.classList.add("hidden");
+  }, 2000);
 }
 
+//footer
 
+function displayFooter() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    document.getElementById("footer").classList.remove("hidden-footer");
+  } else {
+    document.getElementById("footer").classList.add("hidden-footer");
+  }
+}
+const year = new Date().getFullYear();
+function showYear() {
+  document.getElementById("year").innerHTML = year;
+}
+showYear();
 
-
-//to the top button to help with page srolling 
+//to the top button to help with page srolling
 // Get the button:
 let topButton = document.getElementById("topBtn");
 
 // When the user scrolls down 100px from the top of the document, show the button
 
-
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || 
-    document.documentElement.scrollTop > 100) {
-    topButton.style.display = "block";
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    topButton.style.opacity = "1";
   } else {
-    topButton.style.display = "none";
+    topButton.style.opacity = "0";
   }
 }
 
@@ -157,5 +187,4 @@ function scrollFunction() {
 topButton.onclick = function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-  
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,78 @@
-const path = require('path');
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/assets/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '',
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+  entry: "./src/assets/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+    filename: "bundle.js",
+  },
+  mode: "development",
+  devServer: {
+    static: "./dist",
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
             },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "postcss-loader"
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        }
-                    }
-                ]
+          },
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts",
             },
-            {
-                test: /\.(html)$/,
-                 use: ['html-loader'],
-                
-                
-            },
-            {
-                test: /\.(woff|woff2|ttf|otf|eot)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'fonts'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/template.html'), // template file
-            filename: 'index.html', // output file
-            favicon: "./src/assets/img/favicon.png",
-            inject: "body"
-          }) 
-    ]
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template.html"), // template file
+      filename: "index.html", // output file
+      favicon: "./src/assets/img/favicon.png",
+      inject: "body",
+    }),
+  ],
 };
